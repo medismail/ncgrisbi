42bcf9de00940f06b62be79360726c11f1f38134
mmismailm <mmismailm@gmail.com>
2025-07-08 23:20:09 +0000

    Prepare edit transaction

--- src/views/TransactionListView.vue
+++ src/views/TransactionListView.vue
@@ -28,7 +28,7 @@
         <thead>
           <tr>
             <th class="transaction-number">
-              Transaction Number
+              #
             </th>
             <th>Date</th>
             <th>Amount</th>
@@ -39,27 +39,36 @@
             <th>Marked</th>
             <th>Bank Reference</th>
             <th>Split Transaction</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
+          <tr
           <tr
             v-for="(transaction, index) in transactions.transactions.slice().reverse()"
             :key="index"
             :class="{ 'even-row': index % 2 === 0, 'odd-row': index % 2!== 0 }"
           >
             <td><strong>{{ transaction['Transaction Number'] }}</strong></td>
-            <td><strong>{{ transaction.Date }}</strong></td>
-            <td><strong>{{ formatCurrency(transaction.Amount, transaction.Currency, languageCode).formatted }}</strong></td>
-            <td><strong>{{ transaction.Party }}</strong></td>
-            <td><strong>{{ transaction.Category }} ({{ transaction.Subcategory }})</strong></td>
-            <td><strong>{{ transaction['Payment Method'] }}</strong></td>
-            <td><strong>{{ transaction.Note }}</strong></td>
-            <td><strong>{{ transaction.Marked }}</strong></td>
-            <td><strong>{{ transaction['Bank Reference'] }}</strong></td>
-            <td><strong>{{ transaction['Split Transaction'] }}</strong></td>
+            <td><input type="text" v-model="transaction.Date" :disabled="!transaction.isEditing" /></td>
+            <td><input type="number" v-model="transaction.Amount" :disabled="!transaction.isEditing" /></td>
+            <td><input type="text" v-model="transaction.Party" :disabled="!transaction.isEditing" /></td>
+            <td><input type="text" v-model="transaction.Category" :disabled="!transaction.isEditing" /> (<input type="text" v-model="transaction.Subcategory" :disabled="!transaction.isEditing" />)</td>
+            <td><input type="text" v-model="transaction['Payment Method']" :disabled="!transaction.isEditing" /></td>
+            <td><input type="text" v-model="transaction.Note" :disabled="!transaction.isEditing" /></td>
+            <td><input type="checkbox" v-model="transaction.Marked" :disabled="!transaction.isEditing" /></td>
+            <td><input type="text" v-model="transaction['Bank Reference']" :disabled="!transaction.isEditing" /></td>
+            <td><input type="checkbox" v-model="transaction['Split Transaction']" :disabled="!transaction.isEditing" /></td>
+            <td>
+              <button v-if="!transaction.isEditing" @click="transaction.isEditing = true">Edit</button>
+              <button v-else @click="transaction.isEditing = false">Save</button>
+            </td>
           </tr>
         </tbody>
       </table>
+      <button @click="addNewTransaction">
+        Add Transaction
+      </button>
     </template>
   </NcAppContent>
 </template>
@@ -95,7 +104,14 @@ const fetchTransactions = async () => {
   }
   try {
     const response = await fetch('/apps/ncgrisbi/api/account/' + selectedAccountId.value, requestOptions)
-    transactions.value = await response.json()
+    const data = await response.json();
+    // Initialize isEditing property for each transaction
+    if (data && data.transactions) {
+      data.transactions.forEach(transaction => {
+        transaction.isEditing = false;
+      });
+    }
+    transactions.value = data;
   } catch (error) {
     console.error('Failed to fetch transactions:', error)
   }
@@ -110,6 +126,24 @@ watch(() => route.params.id, (newId) => {
   selectedAccountId.value = newId
   fetchTransactions()
 })
+
+const addNewTransaction = () => {
+  const newTransaction = {
+    'Transaction Number': transactions.value.transactions.length + 1, // Simple increment for now
+    Date: '',
+    Amount: 0,
+    Party: '',
+    Category: '',
+    Subcategory: '',
+    'Payment Method': '',
+    Note: '',
+    Marked: false,
+    'Bank Reference': '',
+    'Split Transaction': false,
+    isEditing: true // New transactions are in edit mode by default
+  }
+  transactions.value.transactions.push(newTransaction)
+}
 </script>
 
 <style scoped>
