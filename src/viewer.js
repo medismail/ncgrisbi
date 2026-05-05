7f939c9e97a292222609f8625b3f472c55503aae
Mohamed-Ismail Mejri <imejri@hotmail.com>
2026-05-05 11:05:57 +0200

    fix: Add default property back to FileAction for Nextcloud 33
    - Restored `default: true` so .gsb files open with NCGrisbi by default
    - Updated to use plain object API compatible with @nextcloud/files v4.x
    - Ensures files don't download but open in the app instead

--- src/viewer.js
+++ src/viewer.js
@@ -5,6 +5,7 @@ import { translate as t } from '@nextcloud/l10n'
 const fileAction = {
     id: 'ncgrisbi',
     order: 1,
+    default: true,
     displayName: () => t('ncgrisbi', 'Open With NCGrisbi'),
     iconSvgInline: () => {
         return '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="200" height="200" version="1.0"><defs><linearGradient id="L"><stop offset="0" style="stop-color:#3b4d7f"/><stop offset="1" style="stop-color:#5a92b8"/></linearGradient></defs><rect style="fill:url(#L)" width="200" height="200"/><path style="fill:#fff;fill-opacity:.1" d="M0 0h200v200H0z"/><circle cx="100" cy="100" r="45" style="fill:none;stroke:#fff;stroke-width:8"/><path style="fill:#fff" d="M100 70l12 36h38l-31 22 12 36-31-22-31 22 12-36-31-22h38z"/></svg>'
