630e23557a340acbeb5b46fa4c03c7634e32ee7a
Mohamed-Ismail Mejri <imejri@hotmail.com>
2026-07-24 22:29:17 +0200

    Delete obsolete backend generations and shims

--- lib/bin/ncgrisbi/completion_history.py
+++
@@ -1,5 +0,0 @@
-"""Deprecated compatibility import for snapshot completion precedence."""
-
-from .snapshot_service import prefer_current_account_history
-
-__all__ = ["prefer_current_account_history"]
